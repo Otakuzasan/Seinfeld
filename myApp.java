501292ec13d718a62008c4effb0af73eb84ce7de
Prietoj <70423701+Otakuzasan@users.noreply.github.com>
2023-03-09 13:17:04 -0500

    Add files via upload

---
+++ myApp.java
@@ -0,0 +1,76 @@
+package practice;
+
+import java.util.*; 
+public class myApp{ 
+ public static void main(String[] args) 
+ { 
+   
+  //user prompts 
+  System.out.println("Can you name the main characters from seinfeld?"); 
+  System.out.println("Enter the names seperated by commas"); 
+  //declares the scanner to be used for input 
+  Scanner s = new Scanner(System.in); 
+   
+  //string of names 
+  String seinfeld = "Jerry, Elaine, Kramer, George"; 
+   
+  //splits the string of names 
+  String[] splitter = seinfeld.split(","); 
+   
+  //creates the list 
+  List<String> seinfeldList = Arrays.asList(splitter); 
+   
+   
+  //creates the list as an array 
+  ArrayList<String> sArrayList = new ArrayList<String>(seinfeldList); 
+   
+   
+   
+   
+   
+  ////////////////////////         base list ^ and then user list below  
+   
+   
+   
+   
+  //assigns input to the next Line 
+  String userInput = s.nextLine(); 
+   
+  //creates an array with items split by the comma 
+  String[] useSplit =userInput.split(","); 
+   
+   
+  //creates the list 
+  List<String> userList = Arrays.asList(useSplit); 
+   
+  //creates the array of the list 
+  ArrayList<String>uArrayList = new ArrayList<String>(userList); 
+   
+ 
+ 
+  if(sArrayList.equals(uArrayList)) { 
+   System.out.println("great success"); 
+  }else { 
+   System.out.println("ERROR I NEED SCISSORS 61"); 
+  } 
+   
+   
+   
+   
+   
+  /* 
+  ArrayList<String>userList = new ArrayList<String>(); 
+   
+   
+  userList.add(userInput); 
+  
+ 
+  System.out.println(userList); 
+   
+  */ 
+   
+   
+   
+  s.close(); 
+ } 
+}
